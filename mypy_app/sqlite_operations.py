d83cc0fbab819f57a683272d3e29a56c84395883
Siddhant-Tyagi <sidhant.tyagi@gmail.com>
2014-12-31 17:46:35 +0530

    added the scrollable bar for server list

--- mypy_app/sqlite_operations.py
+++ mypy_app/sqlite_operations.py
@@ -1,12 +1,16 @@
 import sqlite3
 import os
 
-
+#the delete_server_list conatins the mysql server's names to be deleted
+#the method is called in the index view for deleting server
 def delete_servers(delete_server_list):
+    #building the path to the sqlite database file
     sqlite_db_path = os.path.join(os.path.dirname(__file__), os.pardir)
     sqlite_db_path = os.path.abspath(os.path.join(sqlite_db_path, "mypy_app.db"))
+    #using the sqlite3 python API to modify the database
     sqlite_connection_obj = sqlite3.connect(sqlite_db_path)
     sqlite_cursor_obj = sqlite_connection_obj.cursor()
+    #executing commands on the sqlite database for deleting servers
     for server_name in delete_server_list:
         sqlite_cursor_obj.execute("delete from mypy_app_add_server where mysql_server_name='%s'" \
                 %server_name)
--- mypy_app/views.py
+++ mypy_app/views.py
@@ -12,13 +12,16 @@ def adding_server(request):
         form = Add_Server_Form(request.POST)
  
         if form.is_valid():
+            #saving the server details in sqlite db
             form.save(commit=True)
+            #current_object contains the last entry from the sqlite db
             current_object = add_server.objects.all()[len(add_server.objects.all())-1]
+            #calling the connect_to_server method from mypy_mysqldb
+            #attempts to connect to the MySQL server and returns the response message
             adding_server_msg = connect_to_server(current_object)
+            #if the connection to MySQL server fails, delete this entry from the sqlite db
             if "successful" not in adding_server_msg:
                 current_object.delete()
-            print adding_server_msg
-            #return index(request, adding_server_msg)
             return render_to_response('mypy_app/add_server.html', {'form': form, \
                     'add_server_info': adding_server_msg}, context)
         else:
@@ -42,17 +45,12 @@ def index(request):
         delete_server_list = request.POST.getlist('delete_server_checkbox')
         #calling delete_server method from sqlite_operations module
         delete_servers(delete_server_list)
-        
+    
+    #building the updated context from the database
     context_dict = {
             'server_list': add_server.objects.all(),
             }
 
-    #if "successful" not in context_dict['add_server_info']:
-        #current_object.delete()
-
-    """context_dict = {
-            'add_server_info': add_server.objects.all(),
-            }"""
 
     return render_to_response('mypy_app/index.html', context_dict, context)
 
