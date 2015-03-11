c57d986fdde00a10e9c752a5556265805a70a136
Siddhant Tyagi <siddhant.tyagi@webyog.com>
2015-03-11 19:08:07 +0530

    url now redirecting to /mypy/

--- mypy/urls.py
+++ mypy/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 from django.contrib import admin
 
-admin.autodiscover()
+#admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', RedirectView.as_view(url='/mypy/')),  
     url(r'^mypy/', include('mypy_app.urls')),
 )
